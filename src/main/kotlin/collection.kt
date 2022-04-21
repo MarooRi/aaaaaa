597ee992a19d9dff3ec070a5bb05cd8dc00cd90f
MarooRi <faronova0507@gmail.com>
2022-04-21 09:14:17 +0600

    загрузка данных на базу монго

--- src/main/kotlin/collection.kt
+++ src/main/kotlin/collection.kt
@@ -6,36 +6,32 @@ import java.util.*
 
 @Serializable
 data class Student(val name:String,
-                   val idGroup:  @Contextual Id<Group>,
-                   val id: @Contextual Id<Student> = newId())
-
-
+                   val nameGroup: String,
+                   val id: @Contextual Id<Student> = newId()
+)
 
 @Serializable
 data class Group(val nameGroup: String,
-                 @Contextual val idGroup: Id<Group> = newId())
+                 val grades: List<Result> = emptyList()
+)
 
 @Serializable
-data class KindOfWorks(val idWork: @Contextual Id<KindOfWorks> = newId(),
-                       val nameOfWork: String,
-                       val deadLine: @Contextual Date,
+data class KindOfWorks(val nameOfWork: String,
                        val maxCount: Int = 0
+
 )
 
 @Serializable
-data class Result(val idOfWork: @Contextual Id<KindOfWorks>,
+data class Result(val nameOfWork:String,
                   val idStudent: @Contextual Id<Student>,
-                  val date: @Contextual Date,
-                  val count: Int = 0)
-@Serializable
-data class Rule(val idRule: @Contextual Id<Rule> = newId(),
-                val type: String,
-                val idResult: @Contextual Id<Result>,
-                val number: Int = 0
+                  val nameStudent: String,
+                  val count: Int = 0
 )
+/*@Serializable
+data class Rule(val type: String,
+                val number: Int = 0
+)*/
 
 @Serializable
-data class Formula(val idFormula: @Contextual Id<Formula>,
-                   val nameFormula: String,
-                   val listOfRules: List<Rule>)
-
+data class Formula(val nameFormula: String,
+                   val listOfRules: List<Int>)
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,33 @@
 import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
+import org.litote.kmongo.*
 import java.io.FileWriter
 
-fun printInHtml(){
+fun printInHtml() {
     val file = FileWriter("C:/oop/newttt/src/main/kotlin/main.html")
 
     file.appendHTML().html {
         body {
             div {
                 h1 {
-                    + "Добро пожаловать в рейтинг групп от Марии Фароновой"
+                    +"Добро пожаловать в рейтинг групп от Марии Фароновой"
                 }
                 table {
-                    h2 { + "Рейтинг группы" }
+                    h2 { +"Рейтинг группы" }
                     colGroup("Предмет11")
                     tbody {
 
-                        tr{
-                            + "Предмет"
+                        tr {
+                            +"Предмет"
 
                         }
-                        tr{
-                            td{ + "русский"
+                        tr {
+                            td {
+                                +"русский"
                             }
                         }
-                        tr{
-                            td{ + "английский"}
+                        tr {
+                            td { +"английский" }
                         }
 
                     }
@@ -36,10 +38,97 @@ fun printInHtml(){
     file.close()
 }
 
-fun main(){
-    printInHtml()
+// Загружаем виды работ
+fun inWorks(): List<KindOfWorks> {
+    val works = listOf("Lab", "Test", "Course Work").mapIndexed { index, name ->
+        when (index) {
+            0 -> KindOfWorks(name, 5 * (index + 1))
+            1 -> KindOfWorks(name, 5 * (index + 1))
+            else -> KindOfWorks(name, 5 * (index + 1))
+        }
+    }
+    worksMongo.insertMany(works)
+    return works
+}
+
+// Загрущаем на Базу студентов вместе с группами
+fun inStudents(kindOfWorks: List<KindOfWorks>): List<Student> {
+    // Записываем студентов
+    val students = listOf("Maria", "Sasha", "Pasha").mapIndexed { index, name ->
+        if (index < 2)
+            Student(name, "20z")
+        else
+            Student(name, "20m")
+    }
+    studentsMongo.insertMany(students)
+
+
+    val grades = listOf(listOf(5, 10, 15), listOf(3, 4, 10), listOf(5, 1, 15)) // оценки студентов
+    val gradesWithStudents: List<Pair<List<Int>, Student>> = grades.zip(students) { grade, student -> grade to student} // здесь мы соединяем студентов с их оценками
+    val namesGroup = students.mapTo(HashSet()){ it.nameGroup } // берем имена групп
+
+    // соединяем оценки с видом работы
+    val results = gradesWithStudents.map{
+        it.first.mapIndexed{ index, grade ->
+            Result(kindOfWorks[index].nameOfWork, it.second.id, it.second.name, grade)
+        }
+    }.flatMap { it }
+
+    /*val groups3 = students.map{
+        if(it.nameGroup == namesGroup[0])
+            Group(it.nameGroup, results[0])
+    }*/
+
+
+    //filter
+    val groups = namesGroup.map{ name ->
+        val group = students.filter { it.nameGroup == name}
+        println("ГРУППА$group")
+        Group(name, group.map{
+            results.filter {
+                result -> it.name == result.nameStudent
+            }
+        }.flatMap { it }) // если есть студент в группе той, то его добавляем в оценки
+    }
+
+        /*listOf(
+
+        results.filterIndexed { index, res -> res.nameStudent == group[index].name }
+        Group("20z",  results.flatMap { it }.filter { }), Group("20m" ) )*/
+
+    groupsMongo.insertMany(groups)
+    return students
+}
+
+
+fun setKind(student: Student, kindOfWorks: List<KindOfWorks>): List<Result> {
+    val res = kindOfWorks.map{
+        Result(it.nameOfWork, student.id, student.name)
+    }
+    return res
+}
+
+// Коллекци в Mongo
+val studentsMongo = mongoDatabase.getCollection<Student>().apply { drop() }
+val groupsMongo = mongoDatabase.getCollection<Group>().apply { drop() }
+val worksMongo = mongoDatabase.getCollection<KindOfWorks>().apply { drop() }
+
+fun main() {
+    val kindWorks = inWorks()
+    val students = inStudents(kindWorks)
+
+    val grades = listOf(listOf(5, 10, 15), listOf(3, 4, 10), listOf(5, 1, 15))
+
+    /*val x = students.mapIndexed { ind, student ->
+        {
+            kindWorks.mapIndexed { index, kindOfWorks ->
+                groupsMongo.updateMany(and(Group::grades / Result::nameStudent eq student.name), setValue(Group::grades.posOp / Result::nameOfWork, kindOfWorks.nameOfWork))
+            }
+        }
+    }*/
+    /*printInHtml()
     println("Добрый день! Вы попали в программу выставления рейтинга. " +
-            "Вы хотите использовать уже имеющиюся формулу или ввести свою? Если свою, то нажмите 0, если уже имеюся, то 1")
+            "Вы хотите использовать уже имеющиюся формулу или ввести свою? Если свою, то нажмите 0, если уже имеюся, то 1")*/
 
 /*try {
     do {
